0c95c3780f9da3a73fbd818459f962fe9d998d80
Nerijus Arlauskas <nercury@gmail.com>
2019-01-19 16:22:40 +0200

    Update!

--- part01-blink/src/main.rs
+++ part01-blink/src/main.rs
@@ -1,25 +1,20 @@
 #![no_std]
+#![no_main]
 
 extern crate stm32f1;
-extern crate panic_abort;
+extern crate panic_semihosting;
+//extern crate panic_halt;
+extern crate cortex_m_rt;
+extern crate cortex_m_semihosting;
 
-#[allow(non_upper_case_globals, dead_code)]
-const MODE_INPUT: u8 = 0b00;
-#[allow(non_upper_case_globals, dead_code)]
-const MODE_OUTPUT_10MHz: u8 = 0b01;
-#[allow(non_upper_case_globals, dead_code)]
-const MODE_OUTPUT_2MHz: u8 = 0b10;
-#[allow(non_upper_case_globals, dead_code)]
-const MODE_OUTPUT_50MHz: u8 = 0b11;
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
 
-#[allow(dead_code)]
-const CNF_OUTPUT_PUSHPULL: u8 = 0b00;
-#[allow(dead_code)]
-const CNF_INPUT_FLOATING: u8 = 0b01;
-
-fn main() {
+// use `main` as the entry point of this application
+// `main` is not allowed to return
+#[entry]
+fn main() -> ! {
     let peripherals = stm32f1::stm32f100::Peripherals::take().unwrap();
-
     let rcc = peripherals.RCC;
     let port_c = peripherals.GPIOC;
 
@@ -32,11 +27,22 @@ fn main() {
             .mode9().bits(MODE_OUTPUT_10MHz)
             .cnf9().bits(CNF_OUTPUT_PUSHPULL)
     });
+
     port_c.bsrr.write(|w|
         w
             .bs8().set_bit()
             .bs9().set_bit()
     );
 
-    loop {}
+    loop {
+
+    }
 }
+
+const MODE_INPUT: u8 = 0b00;
+const MODE_OUTPUT_10MHz: u8 = 0b01;
+const MODE_OUTPUT_2MHz: u8 = 0b10;
+const MODE_OUTPUT_50MHz: u8 = 0b11;
+
+const CNF_OUTPUT_PUSHPULL: u8 = 0b00;
+const CNF_INPUT_FLOATING: u8 = 0b01;
